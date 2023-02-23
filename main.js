a4da38305c04fed5d1dca816135aa4e925be9092
Josiah Charley <josiah.charley@student.awesomeinc.org>
2023-02-23 22:13:45 +0000

    added some styles and response after win

--- main.js
+++ main.js
@@ -3,8 +3,12 @@ let state = {
     arr: ["","","","","","","","",""],
 
     user: 1,
-    
+    gameState: 0,
+
+    score: [0,0],
+    match: null,
 }
+
 const winConditions = [
     // horizontal win conditions
     [0, 1, 2],
@@ -39,46 +43,52 @@ function make(IdName, Location, elementType, className, content)  {     //functi
 }
 
 function win() {
-    const userIndexes = state.arr.map((e, i) => e === state.user ? i : '').filter(String)
     
+    let userIndexes = state.arr.map((e, i) => e === state.user ? i : '').filter(String)
 
     for(condition of winConditions){  //selecting an array from windConditions  
         let counter = 0;
-        for(num of condition){   //iterating over each value in selected array 
+        let match = [];
+        for(n of condition){   //iterating over each value in selected array 
            
-            if(userIndexes.includes(num) == true){
+            if(userIndexes.includes(n) == true){
                 counter +=1;
-                console.log(counter)
+                match = condition
             }
-            
         }
 
         if(Boolean(counter == 3)){
-            alert(`player${state.user} won!!`)
+            console.log("true")
+            state.gameState = 1;
+            state.score[state.user - 1] +=1;
+            state.match = match //stores matching win Condition in state to use in highlighting function 
+    
+        //if arr does not conain an empty space "" then display that its a tie 
+
         }
     }
 
-   
 }
 
 function buildBoard(){  //whats being updated on page 
     for(i in state.arr){
-     
+    
+        selector("paragraph").innerHTML = `It is player ${state.user}'s turn `; //displays which users turn it is 
+        selector("p1Score").innerHTML = `Player One Score:${state.score[0]}`;
+        selector("p2Score").innerHTML = `Player Two Score:${state.score[1]}`;
 
-        selector("paragraph").innerHTML = `It is player ${state.user}'s turn ` //displays which users turn it is 
         const num = i;
         const newBox = document.createElement("div");
         newBox.setAttribute('id', `${i}`)
-        newBox.setAttribute("class", "col-4 border")
+        newBox.setAttribute("class", "col-4 border display-4 ")
         newBox.innerHTML = state.arr[num];
 
         ///win test 
         //if true execute 
         ////if flase execute bottom
-        function addState(){
+        function changeUser(){
                     //change state and pushes current user to corressponding index 
-                    state.arr[num] = state.user;
-                    if(state.user == 1){
+                    if(state.user == 1){  //changes user 
                         state.arr[num] = 1
                         state.user = 2
                     }
@@ -86,19 +96,29 @@ function buildBoard(){  //whats being updated on page
                         state.arr[num] = 2
                         state.user = 1
                     }
+                 
+        }
+
+        if(state.arr[num] == "" && state.gameState == 0){//add event listener to box only if index is empty(not clicked) or if somebody has won (&& win == false)
+                newBox.addEventListener('click',()=> {
                     
+                    state.arr[num] = state.user; //pushes current user to corressponding index, updating state
+                    win()
                     selector("boardContRow").innerHTML= "";
                     buildBoard()
-                }
+                    if(state.gameState == 1) {
+                        setTimeout("alert(`player${state.user} won`);",600);
 
-        
-       
-        if(state.arr[i] == ""){//add event listener to box only if index is empty(not clicked) or if somebody has won (&& win == false)
-                newBox.addEventListener('click',()=> {
-                    addState()
-                
+                        for(i of state.match){
+                            console.log(i)
+                            selector(i).classList.add("bg-success")
+                        }
+
+                    }
+                    changeUser()
+
                 })//need to add event listener with different function for current index
-            }
+        }
 
         if(state.arr[num] != "") { //if box has content do not add event listener but instead adds x/o as feedback to players to indicate that the area has been pressed 
             if(state.arr[num] == 1){
@@ -108,7 +128,7 @@ function buildBoard(){  //whats being updated on page
                 newBox.innerHTML = "O"
                 }
         }
-        
+
         selector("boardContRow").appendChild(newBox)
     }
     
@@ -117,7 +137,9 @@ function buildBoard(){  //whats being updated on page
 function reset(){
     state.arr = ["","","","","","","","",""]
     state.user = 1;
-    selector("boardContRow").innerHTML= ""
+    state.gameState = 0;
+    state.match = null;
+    selector("boardContRow").innerHTML= "";
 
     buildBoard()
 }
@@ -126,15 +148,20 @@ function reset(){
 function init() {
     make(/*Id:*/"headingContainer",/*Parent Id:*/"app",/*Type:*/"div",/*class:*/"container-fluid text-center",/*Inner Txt:*/"");
         make(/*Id:*/"heading",/*Parent Id:*/"headingContainer",/*Type:*/"h1",/*class:*/"p-3",/*Inner Txt:*/"Tic-Tac-Toe");
-        make(/*Id:*/"paragraph",/*Parent Id:*/"headingContainer",/*Type:*/"p",/*class:*/"p-3",/*Inner Txt:*/"");
-        
-    make(/*Id:*/"boardCont",/*Parent Id:*/"app",/*Type:*/"div",/*class:*/"container h-50",/*Inner Txt:*/"");
+        make(/*Id:*/"paragraph",/*Parent Id:*/"headingContainer",/*Type:*/"p",/*class:*/"p-1",/*Inner Txt:*/"");
+
+    make(/*Id:*/"scoreCont",/*Parent Id:*/"app",/*Type:*/"div",/*class:*/"row d-flex justify-content-center p-3",/*Inner Txt:*/"");
+        make(/*Id:*/"p1Score",/*Parent Id:*/"scoreCont",/*Type:*/"div",/*class:*/"col-4 text-center",/*Inner Txt:*/"");
+        make(/*Id:*/"p2Score",/*Parent Id:*/"scoreCont",/*Type:*/"div",/*class:*/"col-4 text-center",/*Inner Txt:*/"");
+
+
+    make(/*Id:*/"boardCont",/*Parent Id:*/"app",/*Type:*/"div",/*class:*/"container h-50 ",/*Inner Txt:*/"");
         make(/*Id:*/"boardContRow",/*Parent Id:*/"boardCont",/*Type:*/"div",/*class:*/"row text-center h-100",/*Inner Txt:*/"");
-       
+
     buildBoard()
 
-    make(/*Id:*/"buttonCont",/*Parent Id:*/"app",/*Type:*/"div",/*class:*/"container d-flex justify-content-center m-5",/*Inner Txt:*/"");
-        make(/*Id:*/"resetButton",/*Parent Id:*/"buttonCont",/*Type:*/"button",/*class:*/"btn btn-primary",/*Inner Txt:*/"Reset");    
+    make(/*Id:*/"buttonCont",/*Parent Id:*/"app",/*Type:*/"div",/*class:*/"row justify-content-center m-5",/*Inner Txt:*/"");
+        make(/*Id:*/"resetButton",/*Parent Id:*/"buttonCont",/*Type:*/"button",/*class:*/" col-3 btn btn-primary",/*Inner Txt:*/"Reset");    
     selector("resetButton").addEventListener('click', reset )
 }
 
