a560111c9ee06487264c50596b6de0c0bf37713d
Josiah Charley <josiah.charley@student.awesomeinc.org>
2023-02-24 14:52:20 +0000

    added heigh to columns

--- main.js
+++ main.js
@@ -3,27 +3,17 @@ let state = {
     arr: ["","","","","","","","",""],
 
     user: 1,
-    gameState: 0,
+    gameState: 0, //0 = no one has won, 1 = someone has won and upon building page do not include event listeners 
 
     score: [0,0],
     match: null,
 }
 
 const winConditions = [
-    // horizontal win conditions
-    [0, 1, 2],
-    [3, 4, 5],
-    [6, 7, 8],
-  
-    // vertical win conditions
-    [0, 3, 6],
-    [1, 4, 7],
-    [2, 5, 8],
-  
-    // diagonal win conditions
-    [0, 4, 8],
-    [2, 4, 6]
-  ];
+    [0, 1, 2],[3, 4, 5],[6, 7, 8],[0, 3, 6],[1, 4, 7],[2, 5, 8],[0, 4, 8], [2, 4, 6]
+];
+
+
 
 function selector(ElementId){
     return document.getElementById(ElementId)
@@ -42,6 +32,7 @@ function make(IdName, Location, elementType, className, content)  {     //functi
     location.appendChild(newElement)
 }
 
+
 function win() {
     
     let userIndexes = state.arr.map((e, i) => e === state.user ? i : '').filter(String)
@@ -51,7 +42,7 @@ function win() {
         let match = [];
         for(n of condition){   //iterating over each value in selected array 
            
-            if(userIndexes.includes(n) == true){
+            if(userIndexes.includes(n)){
                 counter +=1;
                 match = condition
             }
@@ -60,32 +51,27 @@ function win() {
         if(Boolean(counter == 3)){
             console.log("true")
             state.gameState = 1;
-            state.score[state.user - 1] +=1;
+            state.score[state.user - 1] +=1;// adds to score of current player in state 
             state.match = match //stores matching win Condition in state to use in highlighting function 
-    
-        //if arr does not conain an empty space "" then display that its a tie 
 
+        //if arr does not conain an empty space "" then display that its a tie 
         }
     }
-
 }
 
 function buildBoard(){  //whats being updated on page 
     for(i in state.arr){
     
-        selector("paragraph").innerHTML = `It is player ${state.user}'s turn `; //displays which users turn it is 
+        selector("paragraph").innerHTML = `It is player ${state.user}'s turn `; //displays which users turn it is
         selector("p1Score").innerHTML = `Player One Score:${state.score[0]}`;
         selector("p2Score").innerHTML = `Player Two Score:${state.score[1]}`;
 
         const num = i;
         const newBox = document.createElement("div");
         newBox.setAttribute('id', `${i}`)
-        newBox.setAttribute("class", "col-4 border display-4 ")
+        newBox.setAttribute("class", "col-4 border display-4 tiles")
         newBox.innerHTML = state.arr[num];
-
-        ///win test 
-        //if true execute 
-        ////if flase execute bottom
+        
         function changeUser(){
                     //change state and pushes current user to corressponding index 
                     if(state.user == 1){  //changes user 
@@ -107,15 +93,15 @@ function buildBoard(){  //whats being updated on page
                     selector("boardContRow").innerHTML= "";
                     buildBoard()
                     if(state.gameState == 1) {
-                        setTimeout("alert(`player${state.user} won`);",600);
+                        setTimeout("alert(`player${state.user} won`);",500);
 
                         for(i of state.match){
                             console.log(i)
                             selector(i).classList.add("bg-success")
                         }
-
                     }
                     changeUser()
+                    
 
                 })//need to add event listener with different function for current index
         }
@@ -131,7 +117,6 @@ function buildBoard(){  //whats being updated on page
 
         selector("boardContRow").appendChild(newBox)
     }
-    
 }
 
 function reset(){
@@ -143,7 +128,6 @@ function reset(){
 
     buildBoard()
 }
-
 
 function init() {
     make(/*Id:*/"headingContainer",/*Parent Id:*/"app",/*Type:*/"div",/*class:*/"container-fluid text-center",/*Inner Txt:*/"");
